6ada264040baf4a18de67dec073ef5057974ef53
x0y14 <ya01ma08@gmail.com>
2022-07-09 00:04:50 +0900

    ヘッダー確認用、ID作成の関数を追加

--- util/misc.go
+++ util/misc.go
@@ -2,6 +2,9 @@ package util
 
 import (
 	"encoding/json"
+	"fmt"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -15,4 +18,18 @@ func IsDBJson(str string) bool {
 	hasCurly := strings.Contains(str, "{") && strings.Contains(str, "}")
 	hasSquare := strings.Contains(str, "[") && strings.Contains(str, "]")
 	return IsJSON(str) && (hasCurly || hasSquare)
+}
+
+func ParseBool(s string) bool {
+	b, err := strconv.ParseBool(s)
+	if err != nil {
+		return false
+	}
+	return b
+}
+
+func CreateDirectChatId(userId1 string, userId2 string) string {
+	mem := []string{userId1, userId2}
+	sort.Strings(mem)
+	return fmt.Sprintf("di|%s", strings.Join(mem, "."))
 }
