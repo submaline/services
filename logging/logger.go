b1f065273981f14b489c75959d6a33795a8357e0
x0y14 <ya01ma08@gmail.com>
2022-07-10 17:58:29 +0900

    インフォログの追加と、errorのログレベルをerrorに

--- logging/logger.go
+++ logging/logger.go
@@ -19,10 +19,17 @@ func LogGrpcFuncFinish(l *zap.Logger, serviceName zap.Field, funcName zap.Field)
 }
 
 func LogError(l *zap.Logger, serviceName zap.Field, funcName zap.Field, message string, err error) {
-	l.Info("error",
+	l.Error("error",
 		serviceName,
 		funcName,
 		zap.String("message", message),
 		zap.Errors("detail", []error{err}),
 	)
+}
+
+func LogInfo(l *zap.Logger, serviceName zap.Field, funcName zap.Field, message string) {
+	l.Info("notice",
+		serviceName,
+		funcName,
+		zap.String("message", message))
 }
