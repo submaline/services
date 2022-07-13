9bf42bf6e5de2131278448b3517330315e962ee1
x0y14 <ya01ma08@gmail.com>
2022-07-13 10:40:35 +0900

    user serviceの実装

---
+++ cmd/user/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	firebase "firebase.google.com/go/v4"
+	"fmt"
+	"github.com/bufbuild/connect-go"
+	"github.com/joho/godotenv"
+	"github.com/submaline/services/db"
+	"github.com/submaline/services/gen/supervisor/v1/supervisorv1connect"
+	"github.com/submaline/services/gen/user/v1/userv1connect"
+	"github.com/submaline/services/interceptor"
+	"github.com/submaline/services/server"
+	"go.uber.org/zap"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load env: %v", err)
+	}
+
+	// firebaseの準備
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("failed to setup firebase app: %v", err)
+	}
+
+	// firebase authの準備
+	authClient, err := app.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("failed to setup firebase auth: %v", err)
+	}
+
+	// databaseの準備
+	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("MARIADB_USER"),
+		os.Getenv("MARIADB_PASSWORD"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("MARIADB_DATABASE")))
+	if err != nil {
+		log.Fatalf("failed to setup _db: %v", err)
+	}
+	defer _db.Close()
+	_db.SetConnMaxLifetime(time.Minute * 3)
+	_db.SetMaxOpenConns(10)
+	_db.SetMaxIdleConns(10)
+	dbClient := &db.DBClient{DB: _db}
+
+	// ログ
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("failed to setup zap: %v", err)
+	}
+	defer logger.Sync()
+
+	supervisorClient := supervisorv1connect.NewSupervisorServiceClient(
+		http.DefaultClient,
+		fmt.Sprintf("http://%s:%s", os.Getenv("SUPERVISOR_SERVICE_HOST"), os.Getenv("SUPERVISOR_SERVICE_PORT")),
+	)
+
+	userServer := &server.UserServer{
+		DB:       dbClient,
+		Auth:     authClient,
+		Logger:   logger,
+		SvClient: &supervisorClient,
+	}
+
+	mux := http.NewServeMux()
+	interceptors := connect.WithInterceptors(
+		interceptor.NewAuthInterceptor(authClient, interceptor.AuthPolicy{}))
+	mux.Handle(userv1connect.NewUserServiceHandler(
+		userServer,
+		interceptors,
+	))
+
+	addr := fmt.Sprintf("0.0.0.0:%v", os.Getenv("USER_SERVICE_PORT"))
+
+	// 起動
+	if err := http.ListenAndServe(
+		addr,
+		h2c.NewHandler(mux, &http2.Server{}),
+	); err != nil {
+		log.Fatal(err)
+	}
+}
--- server/user.go
+++ server/user.go
@@ -1,1 +1,88 @@
 package server
+
+import (
+	"context"
+	"firebase.google.com/go/v4/auth"
+	"fmt"
+	"github.com/bufbuild/connect-go"
+	"github.com/submaline/services/db"
+	"github.com/submaline/services/gen/supervisor/v1/supervisorv1connect"
+	userv1 "github.com/submaline/services/gen/user/v1"
+	"github.com/submaline/services/logging"
+	"go.uber.org/zap"
+)
+
+var (
+	UserServiceName = zap.String("serviceName", "User")
+)
+
+type UserServer struct {
+	DB       *db.DBClient
+	Auth     *auth.Client
+	Logger   *zap.Logger
+	SvClient *supervisorv1connect.SupervisorServiceClient
+}
+
+func (s *UserServer) GetAccount(_ context.Context,
+	req *connect.Request[userv1.GetAccountRequest]) (
+	*connect.Response[userv1.GetAccountResponse], error) {
+	funcName := zap.String("funcName", "GetAccount")
+	logging.LogGrpcFuncCall(s.Logger, UserServiceName, funcName)
+	requesterUserId := req.Header().Get("X-Submaline-UserId")
+
+	account, err := s.DB.GetAccount(requesterUserId)
+	if err != nil {
+		logging.LogError(s.Logger, UserServiceName, funcName, "failed to get account data", err)
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	logging.LogGrpcFuncFinish(s.Logger, UserServiceName, funcName)
+	return connect.NewResponse(&userv1.GetAccountResponse{
+		Account: account,
+	}), nil
+}
+
+func (s *UserServer) UpdateAccount(_ context.Context,
+	_ *connect.Request[userv1.UpdateAccountRequest]) (
+	*connect.Response[userv1.UpdateAccountResponse], error) {
+	funcName := zap.String("funcName", "UpdateAccount")
+	logging.LogGrpcFuncCall(s.Logger, UserServiceName, funcName)
+	//requesterUserId := req.Header().Get("X-Submaline-UserId")
+
+	return nil, connect.NewError(connect.CodeUnimplemented, fmt.Errorf("unimplemented: UpdateAccount"))
+}
+
+func (s *UserServer) GetProfile(_ context.Context,
+	req *connect.Request[userv1.GetProfileRequest]) (
+	*connect.Response[userv1.GetProfileResponse], error) {
+	funcName := zap.String("funcName", "GetProfile")
+	logging.LogGrpcFuncCall(s.Logger, UserServiceName, funcName)
+
+	prof, err := s.DB.GetProfile(req.Msg.UserId)
+	if err != nil {
+		logging.LogError(s.Logger, UserServiceName, funcName, "", err)
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	logging.LogGrpcFuncFinish(s.Logger, UserServiceName, funcName)
+	return connect.NewResponse(&userv1.GetProfileResponse{
+		Profile: prof,
+	}), nil
+}
+
+func (s *UserServer) UpdateProfile(_ context.Context,
+	req *connect.Request[userv1.UpdateProfileRequest]) (
+	*connect.Response[userv1.UpdateProfileResponse], error) {
+	funcName := zap.String("funName", "UpdateProfile")
+	logging.LogGrpcFuncCall(s.Logger, UserServiceName, funcName)
+	requesterUserId := req.Header().Get("X-Submaline-UserId")
+
+	prof, err := s.DB.UpdateProfile(requesterUserId, req.Msg)
+	if err != nil {
+		logging.LogError(s.Logger, UserServiceName, funcName, "", err)
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	logging.LogGrpcFuncFinish(s.Logger, UserServiceName, funcName)
+	return connect.NewResponse(&userv1.UpdateProfileResponse{Profile: prof}), nil
+}
