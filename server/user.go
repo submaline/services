4dd9ed61b50fcd966e26b2b0ae3e21eb2dd8e66e
x0y14 <ya01ma08@gmail.com>
2022-07-15 23:10:35 +0900

    user serviceにopを追加

--- server/user.go
+++ server/user.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"github.com/bufbuild/connect-go"
 	"github.com/submaline/services/db"
+	supervisorv1 "github.com/submaline/services/gen/supervisor/v1"
 	"github.com/submaline/services/gen/supervisor/v1/supervisorv1connect"
+	typesv1 "github.com/submaline/services/gen/types/v1"
 	userv1 "github.com/submaline/services/gen/user/v1"
 	"github.com/submaline/services/logging"
+	"github.com/submaline/services/util"
 	"go.uber.org/zap"
 	"log"
 	"os"
@@ -43,6 +46,56 @@ func (s *UserServer) GetAccount(_ context.Context,
 		return nil, connect.NewError(connect.CodeUnknown, err)
 	}
 
+	adminToken, err := util.GenerateToken(os.Getenv("SUBMALINE_ADMIN_FB_EMAIL"), os.Getenv("SUBMALINE_ADMIN_FB_PASSWORD"), false)
+	if err != nil {
+
+		// log
+		if e_ := logging.ErrD(
+			s.Logger,
+			req.Spec().Procedure,
+			err,
+			"管理者トークンの生成に失敗しました",
+			nil,
+			os.Getenv("DISCORD_WEBHOOK_URL")); e_ != nil {
+			log.Println(e_)
+		}
+
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	recordReq := connect.NewRequest(&supervisorv1.RecordOperationRequest{Operations: []*typesv1.Operation{
+		{
+			Id:          0, // svで自動決定
+			Type:        typesv1.OperationType_OPERATION_TYPE_GET_ACCOUNT,
+			Source:      requesterUserId,
+			Destination: []string{requesterUserId},
+			Param1:      "",
+			Param2:      "",
+			Param3:      "",
+			CreatedAt:   nil, // svで自動決定
+		},
+	}})
+	recordReq.Header().Set("Authorization", fmt.Sprintf("Bearer %v", adminToken.IdToken))
+
+	go func() {
+		_, err = (*s.SvClient).RecordOperation(
+			context.Background(),
+			recordReq,
+		)
+		if err != nil {
+			// log
+			if e_ := logging.ErrD(
+				s.Logger,
+				req.Spec().Procedure,
+				err,
+				"Operationの記録に失敗しました",
+				nil,
+				os.Getenv("DISCORD_WEBHOOK_URL")); e_ != nil {
+				log.Println(e_)
+			}
+		}
+	}()
+
 	return connect.NewResponse(&userv1.GetAccountResponse{
 		Account: account,
 	}), nil
@@ -58,6 +111,8 @@ func (s *UserServer) UpdateAccount(_ context.Context,
 func (s *UserServer) GetProfile(_ context.Context,
 	req *connect.Request[userv1.GetProfileRequest]) (
 	*connect.Response[userv1.GetProfileResponse], error) {
+
+	requesterUserId := req.Header().Get("X-Submaline-UserId")
 
 	prof, err := s.DB.GetProfile(req.Msg.UserId)
 	if err != nil {
@@ -75,6 +130,51 @@ func (s *UserServer) GetProfile(_ context.Context,
 
 		return nil, connect.NewError(connect.CodeUnknown, err)
 	}
+
+	adminToken, err := util.GenerateToken(os.Getenv("SUBMALINE_ADMIN_FB_EMAIL"), os.Getenv("SUBMALINE_ADMIN_FB_PASSWORD"), false)
+	if err != nil {
+
+		// log
+		if e_ := logging.ErrD(
+			s.Logger,
+			req.Spec().Procedure,
+			err,
+			"管理者トークンの生成に失敗しました",
+			nil,
+			os.Getenv("DISCORD_WEBHOOK_URL")); e_ != nil {
+			log.Println(e_)
+		}
+
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	recordReq := connect.NewRequest(&supervisorv1.RecordOperationRequest{Operations: []*typesv1.Operation{
+		{
+			Id:          0,
+			Type:        typesv1.OperationType_OPERATION_TYPE_GET_PROFILE,
+			Source:      requesterUserId,
+			Destination: []string{requesterUserId},
+		},
+	}})
+	recordReq.Header().Set("Authorization", fmt.Sprintf("Bearer %s", adminToken.IdToken))
+	go func() {
+		_, err = (*s.SvClient).RecordOperation(
+			context.Background(),
+			recordReq,
+		)
+		if err != nil {
+			// log
+			if e_ := logging.ErrD(
+				s.Logger,
+				req.Spec().Procedure,
+				err,
+				"Operationの記録に失敗しました",
+				nil,
+				os.Getenv("DISCORD_WEBHOOK_URL")); e_ != nil {
+				log.Println(e_)
+			}
+		}
+	}()
 
 	return connect.NewResponse(&userv1.GetProfileResponse{
 		Profile: prof,
@@ -102,6 +202,58 @@ func (s *UserServer) UpdateProfile(_ context.Context,
 
 		return nil, connect.NewError(connect.CodeUnknown, err)
 	}
+
+	adminToken, err := util.GenerateToken(os.Getenv("SUBMALINE_ADMIN_FB_EMAIL"), os.Getenv("SUBMALINE_ADMIN_FB_PASSWORD"), false)
+	if err != nil {
+
+		// log
+		if e_ := logging.ErrD(
+			s.Logger,
+			req.Spec().Procedure,
+			err,
+			"管理者トークンの生成に失敗しました",
+			nil,
+			os.Getenv("DISCORD_WEBHOOK_URL")); e_ != nil {
+			log.Println(e_)
+		}
+
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	// todo : notify friends
+	recordReq := connect.NewRequest(&supervisorv1.RecordOperationRequest{Operations: []*typesv1.Operation{
+		{
+			Id:          0,
+			Type:        typesv1.OperationType_OPERATION_TYPE_UPDATE_PROFILE,
+			Source:      requesterUserId,
+			Destination: []string{requesterUserId},
+		},
+		{
+			Id:          0,
+			Type:        typesv1.OperationType_OPERATION_TYPE_UPDATE_PROFILE_RECV,
+			Source:      requesterUserId,
+			Destination: []string{requesterUserId},
+		},
+	}})
+	recordReq.Header().Set("Authorization", fmt.Sprintf("Bearer %s", adminToken.IdToken))
+	go func() {
+		_, err = (*s.SvClient).RecordOperation(
+			context.Background(),
+			recordReq,
+		)
+		if err != nil {
+			// log
+			if e_ := logging.ErrD(
+				s.Logger,
+				req.Spec().Procedure,
+				err,
+				"Operationの記録に失敗しました",
+				nil,
+				os.Getenv("DISCORD_WEBHOOK_URL")); e_ != nil {
+				log.Println(e_)
+			}
+		}
+	}()
 
 	return connect.NewResponse(&userv1.UpdateProfileResponse{Profile: prof}), nil
 }
